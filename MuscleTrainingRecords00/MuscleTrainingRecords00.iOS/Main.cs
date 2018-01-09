e1237dae7e2f9ba4cba37e3d9be71a55137465ee
燎 石橋 <150461@jc-21.jp>
2018-01-09 14:46:32 +0900

    fix

--- MuscleTrainingRecords00/MuscleTrainingRecords00.iOS/Main.cs
+++ MuscleTrainingRecords00/MuscleTrainingRecords00.iOS/Main.cs
@@ -13,6 +13,7 @@ namespace MuscleTrainingRecords00.iOS
         static void Main(string[] args)
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
+
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
         }
