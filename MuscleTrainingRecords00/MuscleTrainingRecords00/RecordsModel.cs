e3ca9a195d8c5c48d68ca1eaa1e580e8c55f200d
燎 石橋 <150461@jc-21.jp>
2018-01-10 10:05:12 +0900

    fix

--- MuscleTrainingRecords00/MuscleTrainingRecords00/RecordsModel.cs
+++ MuscleTrainingRecords00/MuscleTrainingRecords00/RecordsModel.cs
@@ -14,11 +14,11 @@ namespace MuscleTrainingRecords00
         [PrimaryKey, AutoIncrement, Column("_id")]
         public int M_no { get; set; } //筋トレNo 主キー
 
-        public int M_weight { get; set; } //重量
+        public string M_weight { get; set; } //重量
 
-        public int M_leg { get; set; } //回数
+        public string M_leg { get; set; } //回数
 
-        public int M_set { get; set; } //セット数
+        public string M_set { get; set; } //セット数
 
         public DateTime M_date { get; set; } //日付
 
@@ -26,7 +26,7 @@ namespace MuscleTrainingRecords00
         // public int Set_no { get; set; } //Setting表の外部キー
 
         /********************インサートメソッド**********************/
-        public static void InsertRecords(int m_no, int m_weight, int m_leg, int m_set, DateTime m_date)
+        public static void InsertRecords(string m_weight, string m_leg, string m_set, DateTime m_date)
         {
             //データベースに接続する
             using (SQLiteConnection db = new SQLiteConnection(App.dbPath))
@@ -68,7 +68,7 @@ namespace MuscleTrainingRecords00
         }
 
         /********************デリートメソッド*************************************/
-        public static void DeleteFood(int m_no)
+        public static void DeleteRecords(int m_no)
         {
             //データベースに接続する
             using (SQLiteConnection db = new SQLiteConnection(App.dbPath))
@@ -90,7 +90,7 @@ namespace MuscleTrainingRecords00
         }
 
         /********************オールデリートメソッド*************************************/
-        public static void DeleteAllFood()
+        public static void DeleteAllRecordsd()
         {
             //データベースに接続する
             using (SQLiteConnection db = new SQLiteConnection(App.dbPath))
