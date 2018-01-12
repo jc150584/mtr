c9ba7d76333c6e868ef77ae215031b35607eda45
燎 石橋 <150461@jc-21.jp>
2018-01-12 15:58:14 +0900

    fix

--- MuscleTrainingRecords00/MuscleTrainingRecords00/MenuPage.xaml.cs
+++ MuscleTrainingRecords00/MuscleTrainingRecords00/MenuPage.xaml.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using MuscleTrainingRecords00;
+using MuscleTrainingRecords00.sqlite;
 
 namespace MuscleTrainingRecords00
 {
