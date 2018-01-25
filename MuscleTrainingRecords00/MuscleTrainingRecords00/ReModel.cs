8c41967fa75658d01b3bc454d3866a5780a264d8
燎 石橋 <150461@jc-21.jp>
2018-01-25 13:00:10 +0900

    fix

--- MuscleTrainingRecords00/MuscleTrainingRecords00/ReModel.cs
+++ MuscleTrainingRecords00/MuscleTrainingRecords00/ReModel.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections.ObjectModel;
+using SQLite;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace MuscleTrainingRecords00
 {
