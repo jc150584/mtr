3dfadb4a8e5c858574e84d852b8b2f7fe3eae773
燎 石橋 <150461@jc-21.jp>
2018-01-25 12:46:41 +0900

    fix

--- MuscleTrainingRecords00/MuscleTrainingRecords00/ReModel.cs
+++ MuscleTrainingRecords00/MuscleTrainingRecords00/ReModel.cs
@@ -5,43 +5,45 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
 
-public class ReModel
+namespace MuscleTrainingRecords00
 {
-    public ObservableCollection<Record> Records
+    public class ReModel
     {
-        get;
-        private set;
-    }
-
-    public ReModel()
-    {
-        //今日の日付
-        DateTime now = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
-
-        if (RecordsModel.SelectRecords()!= null)
+        public ObservableCollection<Record> Records
         {
-            var query = RecordsModel.SelectRecords();
-            Records = new ObservableCollection<Record>();
-
-            foreach(var record in query)//拡張forループ
-            {
-                Record r = new Record
-                {
-                    M_no = m_no,
-                    M_name = m_name,
-                    M_weight = m_weight,
-                    M_leg = m_leg,
-                    M_set = m_set,
-                    M_date = m_date
-                };
-                //一行ずつ追加
-                Records.Add(r);
-            }
+            get;
+            private set;
         }
 
-        else
+        public ReModel()
         {
-            Records = new ObservableCollection<Record>
+            //今日の日付
+            DateTime now = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+
+            if (RecordsModel.SelectRecords() != null)
+            {
+                var query = RecordsModel.SelectRecords();
+                Records = new ObservableCollection<Record>();
+
+                foreach (var record in query)//拡張forループ
+                {
+                    Record r = new Record
+                    {
+                        M_no = m_no,
+                        M_name = m_name,
+                        M_weight = m_weight,
+                        M_leg = m_leg,
+                        M_set = m_set,
+                        M_date = m_date
+                    };
+                    //一行ずつ追加
+                    Records.Add(r);
+                }
+            }
+
+            else
+            {
+                Records = new ObservableCollection<Record>
            {
                new Record
                {
@@ -52,23 +54,24 @@ public class ReModel
                    M_date = now
                }
            };
+            }
         }
-    }
 
-    public class Record
-    {
-        public int M_no { get; set; } //筋トレNo 主キー
+        public class Record
+        {
+            public int M_no { get; set; } //筋トレNo 主キー
 
-        public string M_name { get; set; } //筋トレ名前
+            public string M_name { get; set; } //筋トレ名前
 
-        public int M_weight { get; set; } //重量
+            public int M_weight { get; set; } //重量
 
-        public int M_leg { get; set; } //回数
+            public int M_leg { get; set; } //回数
 
-        public int M_set { get; set; } //セット数
+            public int M_set { get; set; } //セット数
 
-        public DateTime M_date { get; set; } //日付
+            public DateTime M_date { get; set; } //日付
 
+        }
     }
 }
 
