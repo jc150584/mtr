47207ef408807f7f5486756b6332c33f8a3cdf44
仁一 伊藤 <150215@jc-21.jp>
2018-01-26 16:16:59 +0900

    fix

--- MuscleTrainingRecords00/MuscleTrainingRecords00/RecordListPage.xaml.cs
+++ MuscleTrainingRecords00/MuscleTrainingRecords00/RecordListPage.xaml.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using MuscleTrainingRecords00;
+using System.Windows.Input;
 
 namespace MuscleTrainingRecords00
 {
