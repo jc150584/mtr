7636262a52f35ec7d09d312e36284a4495b471ae
燎 石橋 <150461@jc-21.jp>
2018-02-01 10:41:02 +0900

    fix

--- MuscleTrainingRecords00/MuscleTrainingRecords00/RecordModelv2.cs
+++ MuscleTrainingRecords00/MuscleTrainingRecords00/RecordModelv2.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using MuscleTrainingRecords00;
 using SQLite;
-using SQLiteNetExtensions.Attributes;
 
 namespace MuscleTrainingRecords00
 {
