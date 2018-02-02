ab4bba0a42deb31b198d3a1fd612395b296f79ae
有希奈 澤田 <150584@jc-21.jp>
2018-02-02 19:05:49 +0900

    fix

--- MuscleTrainingRecords00/MuscleTrainingRecords00/MenudetailPage.xaml.cs
+++ MuscleTrainingRecords00/MuscleTrainingRecords00/MenudetailPage.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Media;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -24,7 +23,7 @@ namespace MuscleTrainingRecords00
 
             Description.Text = d;
 
-            image.Source = Xamarin.Forms.ImageSource.FromResource(i);
+            image.Source = ImageSource.FromResource(i);
 
             t = m;
 
